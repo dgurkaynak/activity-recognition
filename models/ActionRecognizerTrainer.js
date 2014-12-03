fc427a1024506553dda7d908e03692963a351f02
Deniz Gurkaynak <dgurkaynak@gmail.com>
2014-12-03 13:15:40 +0200

    Train automatically with all the datas in traindata folder.

--- models/ActionRecognizerTrainer.js
+++ models/ActionRecognizerTrainer.js
@@ -1,15 +1,15 @@
-var ActionRecognizer = require('./ActionRecognizer');
+var ActionRecognizer = require('./ActionRecognizer'),
+    fs = require('fs'),
+    filePath = './traindata',
+    files = fs.readdirSync(filePath),
+    trainData = {};
 
-var walking = require('../traindata/walking.json'),
-    wavingHands = require('../traindata/waving-hands.json'),
-    wavingLeftHand = require('../traindata/waving-left-hand.json'),
-    wavingRightHand = require('../traindata/waving-right-hand.json');
+files.forEach(function(fileName) {
+    var actionName = fileName.split('.')[0],
+        data = JSON.parse(fs.readFileSync(filePath + '/' + fileName));
 
-var trainData = ActionRecognizer.preprocessTrainData({
-    'walking': walking,
-    'waving-hands': wavingHands,
-    'waving-left-hand': wavingLeftHand,
-    'waving-right-hand': wavingRightHand
+    trainData[actionName] = data;
 });
 
+trainData = ActionRecognizer.preprocessTrainData(trainData);
 ActionRecognizer.train(trainData);
